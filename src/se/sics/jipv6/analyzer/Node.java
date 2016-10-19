9db0f78e64fb1c898c1c3b740ed6533c4bfb7c08
Niclas Finne <nfi@sics.se>
2016-10-19 11:01:26 +0200

    Added convenient methods to set and get node properties

--- src/se/sics/jipv6/analyzer/Node.java
+++ src/se/sics/jipv6/analyzer/Node.java
@@ -3,6 +3,7 @@ package se.sics.jipv6.analyzer;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class Node {
 
@@ -15,6 +16,45 @@ public class Node {
     public int packetReceived;
     public int seqNo; /* the last seqNo of a packet sent towards the node */
 
+    public Object getProperty(String key) {
+        return properties.get(key);
+    }
+
+    public Object getProperty(String key, Object defaultValue) {
+        Object v = properties.get(key);
+        if (v != null) {
+            return v;
+        }
+        return defaultValue;
+    }
+
+    public <T> T getProperty(Class<T> type, String key) {
+        return getProperty(type, key, null);
+    }
+
+    public <T> T getProperty(Class<T> type, String key, T defaultValue) {
+        Object v = this.properties.get(key);
+        if (type.isInstance(v)) {
+            return type.cast(v);
+        }
+        return defaultValue;
+    }
+
+    public Object putProperty(String key, Object value) {
+        if (value == null) {
+            return properties.remove(key);
+        } else {
+            return properties.put(key, value);
+        }
+    }
+
+    public void setProperty(String key, Object value) {
+        if (value == null) {
+            properties.remove(key);
+        } else {
+            properties.put(key, value);
+        }
+    }
 
     public void print(PrintWriter printWriter) {
         printWriter.print("Node - MAC:");
@@ -25,9 +65,10 @@ public class Node {
         for(String ip : ipAddresses) {
             printWriter.print("  " + ip);
         }
-        printWriter.println("\n   Sent/Received => sent: " + packetSent + " recv: " + packetReceived);
-        for(String key : properties.keySet()) {
-            printWriter.println("   " + key + " => " + properties.get(key).toString());
+        printWriter.println();
+        printWriter.println("   Sent/Received => sent: " + packetSent + " recv: " + packetReceived);
+        for(Entry<String,Object> e : properties.entrySet()) {
+            printWriter.println("   " + e.getKey() + " => " + e.getValue());
         }
     }
 
